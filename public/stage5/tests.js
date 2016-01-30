f3999d5819881c899927a028146e69ab2a92e58a
fnwiya <fnwiya@gmail.com>
2016-01-30 18:12:46 +0900

    clear stage5

--- public/stage5/tests.js
+++ public/stage5/tests.js
@@ -9,10 +9,10 @@ describe('ステージ5（意図通りに非同期処理を利用できる）', 
       //
       // ここに下記のコードを記述してください。
       //
-      // promise.then(function(msg) {
-      //   expect(msg).to.equal('resolved!');
-      //   testDone();
-      // });
+      promise.then(function(msg) {
+        expect(msg).to.equal('resolved!');
+        testDone();
+      });
     });
 
 
@@ -26,6 +26,10 @@ describe('ステージ5（意図通りに非同期処理を利用できる）', 
       // testDone();
 
       // ここにコードを記述してください。
+        promise.catch(function (msg) {
+            expect(msg).to.equal('rejected!');
+            testDone();
+        });
 
 
     });
@@ -38,7 +42,7 @@ describe('ステージ5（意図通りに非同期処理を利用できる）', 
       var promise3 = createWaitPromise(messageFragments[2], 30);
 
       // 作成した promise を promise 変数に代入してください。
-      var promise = 'change me!';
+      var promise = Promise.all([promise1, promise2, promise3]);
 
 
       return expect(promise).to.eventually.deep.equal(messageFragments);
@@ -52,7 +56,7 @@ describe('ステージ5（意図通りに非同期処理を利用できる）', 
       var promise3 = createWaitPromise(messageFragments[2], 30);
 
       // 作成した promise を promise 変数に代入してください。
-      var promise = 'change me!';
+      var promise = Promise.race([promise1, promise2, promise3]);
 
 
       return expect(promise).to.eventually.equal(messageFragments[1]);
@@ -69,9 +73,9 @@ describe('ステージ5（意図通りに非同期処理を利用できる）', 
       //
       // ここに下記のコードを記述してください。
       //
-      // var promisedFriends = fetch(api + username).then(function(res) {
-      //   return res.json();
-      // });
+      var promisedFriends = fetch(api + username).then(function(res) {
+        return res.json();
+      });
 
 
       return expect(promisedFriends).to.eventually.have.length(1)
@@ -84,7 +88,9 @@ describe('ステージ5（意図通りに非同期処理を利用できる）', 
       var username = 'Shen';
 
       // 作成した promise を promisedFriends 変数に代入してください。
-      var promisedFriends = 'change me!';
+      var promisedFriends = fetch(api + username).then(function (res) {
+          return res.json();
+      });
 
 
       return expect(promisedFriends).to.eventually.have.length(2)
@@ -97,8 +103,26 @@ describe('ステージ5（意図通りに非同期処理を利用できる）', 
       var username = 'Shen';
 
       // 作成した promise を promisedFriends 変数に代入してください。
-      var promisedFriends = 'change me!';
+      function getFriends(username) {
+          return fetch(api + username)
+          .then(function (res) {
+              return res.json();
+          });
+      }
 
+      function flattenMap(arrayOfArray) {
+          return arrayOfArray.reduce(function (flatArray, array) {
+              return flatArray.concat(array);
+          }, []);
+      }
+
+        var promisedFriends = getFriends(username)
+        .then(function (friends) {
+            return Promise.all(friends.map(getFriends));
+        })
+        .then(function (friendsMap) {
+            return flattenMap(friendsMap);
+        });
 
       return expect(promisedFriends).to.eventually.have.length(1)
         .and.have.members(['TypeScript']);
@@ -127,7 +151,10 @@ describe('ステージ5（意図通りに非同期処理を利用できる）', 
     it('Github の mixi-inc の organization の情報を取得できる', function() {
 
       // 作成した promise を mixiOrg 変数に代入してください。
-      var mixiOrg = 'change me!';
+      var mixiOrg = fetch('https://api.github.com/orgs/mixi-inc')
+        .then(function (res) {
+            return res.json();
+        });
 
       return expect(mixiOrg).to.eventually.have.property('id', 1089312);
 
@@ -140,7 +167,10 @@ describe('ステージ5（意図通りに非同期処理を利用できる）', 
       var repository = 'mixi-inc/JavaScriptTraining';
 
       // 作成した promise を mixiRepo 変数に代入してください。
-      var mixiRepo = 'change me!';
+      var mixiRepo = fetch('https://api.github.com/repos/' + repository)
+        .then(function (res) {
+            return res.json();
+        });
 
 
       return expect(mixiRepo).to.eventually.have.property('full_name', repository);
@@ -153,9 +183,33 @@ describe('ステージ5（意図通りに非同期処理を利用できる）', 
     it('Github API を使って、VimL、Emacs Lisp でスターが最も多いプロダクト名を' +
        'それぞれ 1 つずつ取得できる', function() {
       var languages = [ 'VimL', '"Emacs Lisp"' ];
-      var mostPopularRepos = 'change me!';
 
       // 作成した promise を mostPopularRepos 変数に代入してください。
+      function buildQueryString(queryMap) {
+          return Object.keys(queryMap)
+          .map(function (key) {
+              return encodeURIComponent(key) + '=' + encodeURIComponent(queryMap[key]);
+          })
+              .join('&');
+      }
+           
+      function searchMostPopularRepoByLanguage(lang) {
+          var queryString = buildQueryString({
+              q: 'language:' + lang,
+              sort: 'stars'
+          });
+          return fetch('https://api.github.com/search/repositories?' + queryString)
+          .then(function (response) {
+              return response.json();
+          })
+          .then(function (result) {
+              console.log(result.items[0].full_name);
+              return result.items[0].full_name;
+          });
+      }
+           
+      var mostPopularRepos = Promise.all(
+          languages.map(searchMostPopularRepoByLanguage));
 
 
       return expect(mostPopularRepos).to.eventually.have.length(2)
